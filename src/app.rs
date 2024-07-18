abd29aee5466147b0e894c333ed6c5708eb9e8e6
Zhen Wang <wangzhen31@baidu.com>
2024-07-18 14:33:34 +0800

    Add support for log.

--- src/app.rs
+++ src/app.rs
@@ -30,11 +30,14 @@ pub fn App() -> impl IntoView {
 fn HomePage() -> impl IntoView {
     // Creates a reactive value to update the button
     let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
+    let on_click = move |_| {
+        logging::log!("==> original count = {}", count());
+        set_count.update(|count| *count += 1)
+    };
 
     view! {
         <div class="container flex items-center mt-20 flex-col">
-            <h1 class="text-4xl font-bold text-gray-600 mb-4">"Welcome to AiChat!"</h1>
+            <h1 class="text-4xl font-bold text-cyan-700 mb-4">"Welcome to AiChat!"</h1>
             <div class="">
                 <span class="text-red-400">"Value: " {count} "!"</span>
             </div>
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,19 @@ async fn main() -> std::io::Result<()> {
     use actix_files::Files;
     use actix_web::*;
     use ai_chat::app::*;
+    use dotenv::dotenv;
     use leptos::*;
     use leptos_actix::{generate_route_list, LeptosRoutes};
+    use log::info;
+
+    dotenv().ok();
+    env_logger::init();
 
     let conf = get_configuration(None).await.unwrap();
     let addr = conf.leptos_options.site_addr;
     // Generate the list of routes in your Leptos App
     let routes = generate_route_list(App);
-    println!("listening on http://{}", &addr);
+    info!("listening on http://{}", &addr);
 
     HttpServer::new(move || {
         let leptos_options = &conf.leptos_options;
